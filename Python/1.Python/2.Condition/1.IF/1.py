5719669c92d3b4c5dbd5ccef33f952acfb9a563c
Krishantchauhan <krashantchauhan@gmail.com>
2021-07-26 08:55:01 +0530

    test

--- Python/1.Python/2.Condition/1.IF/1.py
+++ Python/1.Python/2.Condition/1.IF/1.py
@@ -1,9 +1,11 @@
-a=135
-if a>=80:
-    print("A Grade")
-elif a>=60 and a<=79:
-    print("B Grade")
-elif a>=35 and a<=59:
-    print("C Grade")
-else:
-    print("fail")
+# a=135
+# if a>=80:
+#     print("A Grade")
+# elif a>=60 and a<=79:
+#     print("B Grade")
+# elif a>=35 and a<=59:
+#     print("C Grade")
+# else:
+#     print("fail")
+# binary sorted
+def merge            
