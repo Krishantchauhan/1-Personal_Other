493365d607edd47ce7c0dcafd831395674f27c1a
Krishantchauhan <krashantchauhan@gmail.com>
2022-06-09 21:38:06 +0530

    GFG array

---
+++ 1-Coding/GFG/3-Array-string-pointer/1-size_of_array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+  dou arr[10]={10,20,30,40,50};
+
+  int a=sizeof(arr)/sizeof(arr[0]);
+  // sizeof(arr) return 4*5=20
+  //  sizeof(arr[0]) return 4bytes
+  //Then 20/4==5
+
+  printf("The size is = %d \n",a);
+
+}
