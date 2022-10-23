40ca24e29741aa6b6c541013a40bfd3b915045c5
Krishantchauhan <krashantchauhan@gmail.com>
2022-10-23 15:39:56 +0530

    check

---
+++ a.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ void main()
+   {
+  int a[100][100],i,j,r,c, ;
+  printf("Enter the column of an array\n");
+  scanf("%d",&c);
+  printf("Enter the rows of an array\n");
+  scanf("%d",&r);
+  printf("Enter the  Elements\n");
+
+  while () 
+  {
+
+  }
+
+
+
+}
