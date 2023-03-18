70b4a33b0679fcdb864af01fff53592b6c2c812c
Krishantchauhan <krashantchauhan@gmail.com>
2023-03-18 11:25:44 +0530

    rev

--- 1-Coding/GFG/3-Array-string-pointer/1-Array/Operations/5_Reverse.c
+++ 1-Coding/GFG/3-Array-string-pointer/1-Array/Operations/5_Reverse.c
@@ -3,7 +3,7 @@ int main()
 {
     int a[] = {12, 34, 10, 6, 40};
     int low = 0, high = 4;
-    while (low < high)
+    while (low != high)
     {
         int temp = a[low];
         a[low] = a[high];
