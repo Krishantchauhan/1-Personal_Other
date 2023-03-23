5a474f2281f861900ce0447f66471e42afa2acf3
Krishantchauhan <krashantchauhan@gmail.com>
2023-03-23 20:00:17 +0530

    diagonal swap

--- 1-Coding/GFG/Matrix/diagonal.c
+++ 1-Coding/GFG/Matrix/diagonal.c
@@ -4,28 +4,70 @@ int main()
     int r, c;
     scanf("%d %d", &r, &c);
     int low = 0, high = c - 1, temp = 0;
+    // int a[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+    // int a[5][5] = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}, {21, 22, 23, 24, 25}};
+    int a[r][c];
     for (int i = 0; i < r; i++)
     {
-
-        if (low <= high)
+        for (int j = 0; j < c; j++)
         {
-            temp = a[i][low];
-            a[i][low] = a[i][high];
-            a[i][high] = temp;
-            if (i < r / 2)
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    if (r % 2 == 0)
+    {
+        low = -1;
+        high = c;
+    }
+    if (r != c)
+    {
+        printf("Not Possible");
+    }
+    else
+    {
+        for (int i = 0; i < r; i++)
+        {
+            if (r % 2 == 0)
             {
-                low++;
-                high--;
+                if (1)
+                {
+                    if (i < r / 2)
+                    {
+                        low++;
+                        high--;
+                    }
+                    if (i > r / 2)
+                    {
+                        low--;
+                        high++;
+                    }
+                    temp = a[i][low];
+                    a[i][low] = a[i][high];
+                    a[i][high] = temp;
+                }
             }
-            else
+            if (r % 2 != 0)
             {
-                low--;
-                high++;
+                if (low <= high)
+                {
+                    temp = a[i][low];
+                    a[i][low] = a[i][high];
+                    a[i][high] = temp;
+                    if (i < r / 2)
+                    {
+                        low++;
+                        high--;
+                    }
+                    else
+                    {
+                        low--;
+                        high++;
+                    }
+                }
             }
         }
     }
-    printf("%d %d", low, high);
-    printf("\n");
     for (int i = 0; i < r; i++)
     {
         for (int j = 0; j < c; j++)
