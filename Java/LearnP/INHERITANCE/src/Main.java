6e19624c3553895391533fb6af03df8308678fef
Krishantchauhan <krashantchauhan@gmail.com>
2023-03-29 08:49:13 +0530

    Inheritance
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

---
+++ Java/LearnP/INHERITANCE/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Animal animal = new Animal("Genric Animal","Huge",500);
+        doAnimalStuff(animal,"fast");
+
+        Dog dog = new Dog();
+        doAnimalStuff(dog,"fast");
+
+        Dog yorki = new Dog("Yorki",15);
+        doAnimalStuff(yorki,"fast");
+
+        Dog retriever = new Dog("Labrador Retriever",65,"Floppy","Swimmer");
+        doAnimalStuff(retriever,"slow");
+
+        Dog wolf = new Dog("Wolf",40);
+        doAnimalStuff(wolf,"slow");
+
+
+    }
+
+    public static void doAnimalStuff(Animal animal,String speed){
+        animal.makeNoise();
+        animal.move(speed);
+        System.out.println(animal);
+        System.out.println("_ _ _ _");
+    }
+}
