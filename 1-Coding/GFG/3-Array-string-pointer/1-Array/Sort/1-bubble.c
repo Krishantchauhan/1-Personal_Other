dbdb70b653854aff303dbc1f9e297d2de1ff8ebe
Krishantchauhan <krashantchauhan@gmail.com>
2023-04-09 12:09:32 +0530

    moved to another
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- 1-Coding/GFG/3-Array-string-pointer/1-Array/Sort/1-bubble.c
+++ 1-Coding/GFG/3-Array-string-pointer/1-Array/Sort/1-bubble.c
@@ -9,14 +9,21 @@ void swap(int *a, int *b)
 
 int main()
 {
-    int s = 7;
-    int a[7] = {1, 63, 10, 6, 47, 88, 3};
+    // int s = 7;
+    // int a[7] = {1, 63, 10, 6, 47, 88, 3};
+
+    int s = 25;
+    int a[s];
+    for (int i = 0; i < s; i++)
+    {
+        scanf("%d", &a[i]);
+    }
 
     for (int i = 0; i < s - 1; i++)
     {
         for (int j = 0; j < s - i - 1; j++)
         {
-            printf("%d", j);
+            // printf("%d", j);
             if (a[j] > a[j + 1])
                 swap(&a[j], &a[j + 1]);
         }
--- 1-Coding/GFG/DSA/tempCodeRunnerFile.c
+++
@@ -1,8 +0,0 @@
-    for(int i=0;i<4;i++)
-    {
-        for(int j=0;j<10;j++)
-        {
-            printf("%d ",b[i][j]);       
-        }
-        printf("\n");
-    }
