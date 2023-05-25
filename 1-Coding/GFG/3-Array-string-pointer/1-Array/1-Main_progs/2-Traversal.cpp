be0426fdf4408380f087d5ebc3f94fd3e153f330
Krishantchauhan <krashantchauhan@gmail.com>
2023-05-25 21:32:47 +0530

    ..
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- 1-Coding/GFG/3-Array-string-pointer/1-Array/1-Main_progs/2-Traversal.cpp
+++ 1-Coding/GFG/3-Array-string-pointer/1-Array/1-Main_progs/2-Traversal.cpp
@@ -1,22 +1,21 @@
 #include <stdio.h>
 
+int main()
+{
+  int a[] = {10, 20, 30, 40, 50};
+  int n = sizeof(a) / sizeof(a[0]);
 
- int main()
- {
-  int a[]={10,20,30,40,50};
-  int n=sizeof(a)/sizeof(a[0]);
-
-  //Normal For Loop
+  // Normal For Loop
   printf("Noraml For Loop \n");
-  for(int i=0;i<n;i++)
+  for (int i = 0; i < n; i++)
   {
-    printf("%d \t",a[i]);
+    printf("%d \t", a[i]);
   }
 
-  //Range Based For Loop
+  // Range Based For Loop
 
   printf("\nRange Based For Loop \n");
-  for(int i:a)
-  printf("%d \t",i);
+  for (int i : a)
+    printf("%d \t", i);
   printf("\n");
 }
