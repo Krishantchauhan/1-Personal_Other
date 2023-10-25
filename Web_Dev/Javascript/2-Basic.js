a7ad1ff82db480b40ddbf7ca12ccc93c34ac3035
Krishantchauhan <krashantchauhan@gmail.com>
2023-10-25 19:53:43 +0530

    Reviewing Functions
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- Web_Dev/Javascript/2-Basic.js
+++ Web_Dev/Javascript/2-Basic.js
@@ -90,3 +90,21 @@ console.log(fruitProcessor(2, 3));
 
 
 
+///////////////////////////////////////
+// Reviewing Functions
+const calcAge = function (birthYeah) {
+  return 2037 - birthYeah;
+}
+
+const yearsUntilRetirement = function (birthYeah, firstName) {
+  const age = calcAge(birthYeah);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired 🎉`);
+    return -1;
+  }
+}
