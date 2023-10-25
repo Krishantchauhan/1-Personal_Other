01d0f6ca98bb558c489bfd9751df0640dbf0ff72
Krishantchauhan <krashantchauhan@gmail.com>
2023-10-25 19:53:34 +0530

    ..
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- Web_Dev/Javascript/2-Basic.js
+++ Web_Dev/Javascript/2-Basic.js
@@ -88,22 +88,5 @@ function fruitProcessor(apples, oranges) {
 }
 console.log(fruitProcessor(2, 3));
 
-///////////////////////////////////////
-// Reviewing Functions
-const calcAge = function (birthYeah) {
-  return 2037 - birthYeah;
-}
 
-const yearsUntilRetirement = function (birthYeah, firstName) {
-  const age = calcAge(birthYeah);
-  const retirement = 65 - age;
-
-  if (retirement > 0) {
-    console.log(`${firstName} retires in ${retirement} years`);
-    return retirement;
-  } else {
-    console.log(`${firstName} has already retired 🎉`);
-    return -1;
-  }
-}
 
