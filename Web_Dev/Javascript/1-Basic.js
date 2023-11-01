792a009da01b4a815256285cee2d732db35b78f6
Krishantchauhan <krashantchauhan@gmail.com>
2023-11-01 19:26:07 +0530

    basic.js
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- Web_Dev/Javascript/1-Basic.js
+++ Web_Dev/Javascript/1-Basic.js
@@ -1,31 +1,31 @@
 ////////////////////////////////////
 // Linking a JavaScript File
-let js = "amazing";
+let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
 ////////////////////////////////////
 // Values and Variables
-console.log("Jonas");
+console.log('Krishant');
 console.log(23);
 
-let firstName = "Matilda";
+let firstName = 'Krishant';
 
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
 // Variable name conventions
-let jonas_matilda = "JM";
+let K_S = 'KS';
 let $function = 27;
 
-let person = "jonas";
+let person = 'jonas';
 let PI = 3.1415;
 
-let myFirstJob = "Coder";
-let myCurrentJob = "Teacher";
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
 
-let job1 = "programmer";
-let job2 = "teacher";
+let job1 = 'programmer';
+let job2 = 'teacher';
 
 console.log(myFirstJob);
 
@@ -39,7 +39,7 @@ console.log(typeof javascriptIsFun);
 // console.log(typeof 23);
 // console.log(typeof 'Jonas');
 
-javascriptIsFun = "YES!";
+javascriptIsFun = 'YES!';
 console.log(typeof javascriptIsFun);
 
 let year;
@@ -60,10 +60,10 @@ const birthYear = 1991;
 // birthYear = 1990;
 // const job;
 
-var job = "programmer";
-job = "teacher";
+var job = 'programmer';
+job = 'teacher';
 
-lastName = "Schmedtmann";
+lastName = 'Schmedtmann';
 console.log(lastName);
 
 ////////////////////////////////////
@@ -77,9 +77,9 @@ console.log(ageJonas, ageSarah);
 console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
 // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
 
-const firstName = "Jonas";
-const lastName = "Schmedtmann";
-console.log(firstName + " " + lastName);
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
 
 // Assignment operators
 let x = 10 + 5; // 15
@@ -115,13 +115,13 @@ console.log(ageJonas, ageSarah, averageAge);
 
 ////////////////////////////////////
 // Strings and Template Literals
-const firstName = "Jonas";
-const job = "teacher";
+const firstName = 'Jonas';
+const job = 'teacher';
 const birthYear = 1991;
 const year = 2037;
 
 const jonas =
-  "I'm " + firstName + ", a " + (year - birthYear) + " year old " + job + "!";
+  "I'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + job + '!';
 console.log(jonas);
 
 const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
@@ -130,9 +130,9 @@ console.log(jonasNew);
 console.log(`Just a regular string...`);
 
 console.log(
-  "String with \n\
+  'String with \n\
 multiple \n\
-lines"
+lines'
 );
 
 console.log(`String
@@ -144,7 +144,7 @@ lines`);
 const age = 15;
 
 if (age >= 18) {
-  console.log("Sarah can start driving license 🚗");
+  console.log('Sarah can start driving license 🚗');
 } else {
   const yearsLeft = 18 - age;
   console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
@@ -164,30 +164,30 @@ console.log(century);
 // Type Conversion and Coercion
 
 // type conversion
-const inputYear = "1991";
+const inputYear = '1991';
 console.log(Number(inputYear), inputYear);
 console.log(Number(inputYear) + 18);
 
-console.log(Number("Jonas"));
+console.log(Number('Jonas'));
 console.log(typeof NaN);
 
 console.log(String(23), 23);
 
 // type coercion
-console.log("I am " + 23 + " years old");
-console.log("23" - "10" - 3);
-console.log("23" / "2");
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' / '2');
 
-let n = "1" + 1; // '11'
+let n = '1' + 1; // '11'
 n = n - 1;
 console.log(n);
 
 ////////////////////////////////////
 // Equality Operators: == vs. ===
-const age = "18";
-if (age === 18) console.log("You just became an adult :D (strict)");
+const age = '18';
+if (age === 18) console.log('You just became an adult :D (strict)');
 
-if (age == 18) console.log("You just became an adult :D (loose)");
+if (age == 18) console.log('You just became an adult :D (loose)');
 
 const favourite = Number(prompt("What's your favourite number?"));
 console.log(favourite);
@@ -195,57 +195,57 @@ console.log(typeof favourite);
 
 if (favourite === 23) {
   // 22 === 23 -> FALSE
-  console.log("Cool! 23 is an amzaing number!");
+  console.log('Cool! 23 is an amzaing number!');
 } else if (favourite === 7) {
-  console.log("7 is also a cool number");
+  console.log('7 is also a cool number');
 } else if (favourite === 9) {
-  console.log("9 is also a cool number");
+  console.log('9 is also a cool number');
 } else {
-  console.log("Number is not 23 or 7 or 9");
+  console.log('Number is not 23 or 7 or 9');
 }
 
-if (favourite !== 23) console.log("Why not 23?");
+if (favourite !== 23) console.log('Why not 23?');
 
 ////////////////////////////////////
 // The switch Statement
-const day = "friday";
+const day = 'friday';
 
 switch (day) {
-  case "monday": // day === 'monday'
-    console.log("Plan course structure");
-    console.log("Go to coding meetup");
+  case 'monday': // day === 'monday'
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
     break;
-  case "tuesday":
-    console.log("Prepare theory videos");
+  case 'tuesday':
+    console.log('Prepare theory videos');
     break;
-  case "wednesday":
-  case "thursday":
-    console.log("Write code examples");
+  case 'wednesday':
+  case 'thursday':
+    console.log('Write code examples');
     break;
-  case "friday":
-    console.log("Record videos");
+  case 'friday':
+    console.log('Record videos');
     break;
-  case "saturday":
-  case "sunday":
-    console.log("Enjoy the weekend :D");
+  case 'saturday':
+  case 'sunday':
+    console.log('Enjoy the weekend :D');
     break;
   default:
-    console.log("Not a valid day!");
+    console.log('Not a valid day!');
 }
 
-if (day === "monday") {
-  console.log("Plan course structure");
-  console.log("Go to coding meetup");
-} else if (day === "tuesday") {
-  console.log("Prepare theory videos");
-} else if (day === "wednesday" || day === "thursday") {
-  console.log("Write code examples");
-} else if (day === "friday") {
-  console.log("Record videos");
-} else if (day === "saturday" || day === "sunday") {
-  console.log("Enjoy the weekend :D");
+if (day === 'monday') {
+  console.log('Plan course structure');
+  console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+  console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+  console.log('Write code examples');
+} else if (day === 'friday') {
+  console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+  console.log('Enjoy the weekend :D');
 } else {
-  console.log("Not a valid day!");
+  console.log('Not a valid day!');
 }
 
 ////////////////////////////////////
@@ -255,10 +255,10 @@ if (day === "monday") {
 true && false && !false;
 
 if (23 > 10) {
-  const str = "23 is bigger";
+  const str = '23 is bigger';
 }
 
-const me = "Jonas";
+const me = 'Jonas';
 console.log(`I'm ${2037 - 1991} years old ${me}`);
 
 ////////////////////////////////////
@@ -266,15 +266,15 @@ console.log(`I'm ${2037 - 1991} years old ${me}`);
 const age = 23;
 // age >= 18 ? console.log('I like to drink wine 🍷') : console.log('I like to drink water 💧');
 
-const drink = age >= 18 ? "wine 🍷" : "water 💧";
+const drink = age >= 18 ? 'wine 🍷' : 'water 💧';
 console.log(drink);
 
 let drink2;
 if (age >= 18) {
-  drink2 = "wine 🍷";
+  drink2 = 'wine 🍷';
 } else {
-  drink2 = "water 💧";
+  drink2 = 'water 💧';
 }
 console.log(drink2);
 
-console.log(`I like to drink ${age >= 18 ? "wine 🍷" : "water 💧"}`);
+console.log(`I like to drink ${age >= 18 ? 'wine 🍷' : 'water 💧'}`);
